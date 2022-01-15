c8041846adba9a32690c3ada0694a90539e2dfc8
Costinteo <costinteo@gmail.com>
2022-01-15 22:01:02 +0200

    First version of emojifier

---
+++ 😀.h
@@ -0,0 +1,27 @@
+#define 🚪 main
+#define 📩 printf
+#define 🪦 return
+#define 👍 true
+#define 👎 false
+
+#define 🥚 0
+#define 🍷 1
+#define 🆕 "\n"
+#define 🌟 *
+#define 💦 ;
+#define 👉 (
+#define 👈 )
+#define 🌜 {
+#define 🌛 }
+
+
+#define 🧰 struct
+#define 🔢 int
+#define 💀 void
+#define 👌 bool
+#define 🔤 char
+
+
+
+
+
